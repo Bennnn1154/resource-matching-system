e498ea8f7ec0e25334fada9c6f214c183078be36
Bennnn1154 <s111453005@mail1.ncnu.edu.tw>
2025-07-24 22:54:37 +0800

    from one to two

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -143,5 +143,5 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://resource-matching-system-1.onrender.com",
-    'resource-matching-system-1.onrender.com'
+    "resource-matching-system-1.onrender.com"
 ]
